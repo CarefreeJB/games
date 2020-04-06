143794709ee80be228e9a41fa030bc71b3696b6d
CarefreeJB <63158288+CarefreeJB@users.noreply.github.com>
2020-04-06 19:37:44 +0545

    george

---
+++ george.py
@@ -0,0 +1,12 @@
+
+character_name = "George"
+character_age = "50"
+print("There once was a man named " + character_name +", ")
+print("he was " + character_age + ", years old. ")
+print("He really liked the name " + character_name +", ")
+print("but didn't like being " + character_age + ".")
+
+character_name = "Mike"
+character_age = "90"
+print("He really liked the name " + character_name +", ")
+print("but didn't like being " + character_age + ", ")
