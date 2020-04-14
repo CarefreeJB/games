7b84411d726046870b230aa2be605bddac2c9652
CarefreeJB <63158288+CarefreeJB@users.noreply.github.com>
2020-04-14 09:57:08 +0545

    score added

--- pong.py
+++ pong.py
@@ -1,10 +1,14 @@
 import turtle
 
 wn = turtle.Screen()
-wn.title("PONG BY CAREFREE")
-wn.bgcolor("blue")
+wn.title("ASHU IS DONKEY")
+wn.bgcolor("red")
 wn.setup(width=800, height=600)
 wn.tracer(0)
+
+# Score
+score_a = 0
+score_b = 0
 
 # Paddle A
 paddle_a = turtle.Turtle()
@@ -31,8 +35,17 @@ ball.shape("circle")
 ball.color("black")
 ball.penup()
 ball.goto(0, 0)
-ball.dx = 1
-ball.dy = -1
+ball.dx = 1.5
+ball.dy = -1.5
+
+# Pen
+pen = turtle. Turtle()
+pen.speed (0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B: 0", align="center", font=("Courier", 24, "normal"))
 
 # Function
 def paddle_a_up():
@@ -82,10 +95,16 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0, 0)
         ball.dx *= -1
+        score_b +=1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     # Paddle and ball collisions
     if (ball.xcor() > 340 and ball.xcor() < 350) and ( ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() -40):
